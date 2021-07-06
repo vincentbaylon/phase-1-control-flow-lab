72ab89957d82291e7e41799ce0b2110037e437ce
vincentbaylon <vincentbaylon@gmail.com>
2021-07-05 20:57:51 -0700

    commit tree 50a78f878e729294fe1ee9939564a67a8d097c82

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue < 400) {
+    return 'This one is on me!'
+  } else if (someValue > 2500) {
+    return 'No can do.'
+  } else if (someValue > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const isNewYork = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return isNewYork;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.'
+  }
 }
